f2524d90dc8504b05bd794656e8eca57cf89167d
Arancha <arancha.ramirez21@imperial.ac.uk>
2021-10-29 17:15:03 +0100

    Update Exercise_6.py

--- Exercise_6.py
+++ Exercise_6.py
@@ -4,3 +4,14 @@
 # value of the difference between 24 and the input.
 # Example 1: input = 5, output = 19
 # Example 2: input = 26, output = 8
+a = int(input(""))
+
+if a > 24:
+    diff = a -24
+else:
+    diff = 24 - a
+print("the difference is:", diff)
+cube = diff*diff*diff
+print("the cube is:", cube)
+
+#FINISHED AND WORKS CORRECTLY, CHECKED
