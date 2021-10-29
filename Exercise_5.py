65d1fd8f620d5be45672763935e2c57d21bac94e
Arancha <arancha.ramirez21@imperial.ac.uk>
2021-10-29 17:13:27 +0100

    Update Exercise_5.py

--- Exercise_5.py
+++ Exercise_5.py
@@ -4,3 +4,14 @@
 # another integer. The program will keep asking for a number, until the length of the string and the value
 # of the integer match each other.
 # Example: string = "hello", integer = 4, output --> "Please provide another number"
+a = int(input("number:"))
+b = input("word:")
+
+
+while len(b) != a:
+    print("Please provide another number" ,)
+    a = int(input("new number:"))
+else:
+    print("the number and word coincide in length")
+
+#FINISHED AND WORKS CORRECTLY, CHECKED
