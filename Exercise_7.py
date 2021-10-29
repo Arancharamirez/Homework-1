ea778289ae94db2ac9268d648d758abf9df64275
Arancha <arancha.ramirez21@imperial.ac.uk>
2021-10-29 17:15:52 +0100

    Update Exercise_7.py

--- Exercise_7.py
+++ Exercise_7.py
@@ -3,3 +3,20 @@
 # this number and print it on the screen. If the number is negative the program will print 
 # "Factorial does not exist for negative numbers". Remember that the factorial of 0 is 1.
 # Example: input = 5, output = 5*4*3*2*1 = 120
+
+def factorial(a):
+    if a == 1:
+        return 1
+    else:
+        return a * factorial(a-1)
+
+a = int(input(""))
+
+if a < 0:
+    print("Factorial does not exit for negative numbers")
+if a == 0:
+    print("The factorial of 0 is 1")
+else:
+    print("The factorial of", a,"is:", factorial(a))
+
+#FINISHED AND WORKS CORRECTLY, CHECKED
