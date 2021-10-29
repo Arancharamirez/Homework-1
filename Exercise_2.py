19f358957985bcd2b2d66f14e9c66324307ed228
Arancha <arancha.ramirez21@imperial.ac.uk>
2021-10-29 17:11:00 +0100

    Update Exercise_2.py

--- Exercise_2.py
+++ Exercise_2.py
@@ -7,3 +7,19 @@
 # example 2: n = 3, p = 6, output = 3*33*333*3333*33333*333333 = 1220864470355308779
 
 
+def multiplication(n,p):
+    str_n=str(n)
+    multiplication = n
+    multiplication_of_all_strings = str(n)
+    for i in range(1,p):
+        multiplication_of_all_strings = multiplication_of_all_strings + str_n
+        multiplication = multiplication * int(multiplication_of_all_strings)
+    return multiplication
+
+n = int(input("n:"))
+p = int(input("p:"))
+
+mul_of_no = multiplication(n,p)
+print("mul:",mul_of_no)
+
+#FINISHED AND WORKS CORRECTLY, CHECKED
